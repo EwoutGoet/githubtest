da69d61e998b2fe49d2f0b454b8f2aed435c5925
Ewout Goet <ewoutdg@gmail.com>
2020-11-24 14:21:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 print("Goodbye world")
+
+
+def something(yes):
+    print("Some world")
+
+def Everything(no):
+    print("You're my world")
+
+def main():
+    i = 1
+    something(i)
+    Everything(i)
+
+
+if __name__ == "__main__":
+    main()
